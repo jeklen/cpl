f4bdc57f03635e839e0b72913d4ea653561855dc
zhql0907@outlook.com <zhql0907@outlook.com>
2017-05-01 19:00:05 +0800

    first commit

---
+++ ch4/1countprint.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int max);
+int strindex(char source[], char search for[]);
+
+char pattern[] = "ould";    /* pattern to search for */
